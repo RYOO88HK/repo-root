8986311abf0cdd580d64157e7cf6b2e373e2ee5c
RYOO88HK <ryoo88201323@gmail.com>
2026-01-13 11:41:44 +0900

    #1. 첫번째 배포

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -3,3 +3,5 @@ import axios from 'axios'
 // 생산환경(Nginx)에서는 /api가 백엔드로 프록시됩니다.
 // 로컬 개발은 vite proxy(8080) 사용.
 export const api = axios.create({ baseURL: '/api' })
+
+export const api2 = axios.create({ baseURL: '/test' })
